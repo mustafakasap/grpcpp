15b9a91444b0b0335918e1a2c64f7f145a607633
mustafakasap <m@mustafakasap.com>
2021-07-22 05:12:03 +0000

    init

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,55 +1,208 @@
 #include <csignal>
 #include <iostream>
 
-#include "log_helper.h"
-#include "gclient.h"
+#include <memory>
+#include <string>
+#include <thread>
+#include <grpc++/grpc++.h>
+#include "client_server_bidi_async_stream.grpc.pb.h"
 
 using namespace std;
+using namespace client_server::grpc::v1;
 
-extern int g_system_log_level;
+using grpc::Channel;
+using grpc::ClientAsyncReaderWriter;
+using grpc::ClientContext;
+using grpc::CompletionQueue;
+using grpc::Status;
+
+enum class Type
+{
+	READ = 1,
+	WRITE = 2,
+	CONNECT = 3,
+	DONE = 4,
+	FINISH = 5
+};
+
+class AsyncBidiBiDiStreamClient
+{
+public:
+	explicit AsyncBidiBiDiStreamClient(std::shared_ptr<Channel> channel)
+		: stub_(CSService01::NewStub(channel))
+	{
+		grpc_thread_.reset(
+			new std::thread(std::bind(&AsyncBidiBiDiStreamClient::GrpcThread, this)));
+		stream_ = stub_->AsyncCSServiceMethod01(&context_, &cq_, reinterpret_cast<void *>(Type::CONNECT));
+	}
+
+	~AsyncBidiBiDiStreamClient()
+	{
+		std::cout << "Shutting down client...." << std::endl;
+		grpc::Status status;
+		cq_.Shutdown();
+		grpc_thread_->join();
+	}
+
+	// Similar to the async hello example in greeter_async_client but does not
+	// wait for the response. Instead queues up a tag in the completion queue
+	// that is notified when the server responds back (or when the stream is
+	// closed). Returns false when the stream is requested to be closed.
+	bool AsyncSayHello(const std::string &user)
+	{
+		if (user == "quit")
+		{
+			stream_->WritesDone(reinterpret_cast<void *>(Type::DONE));
+			return true;
+		}
+
+		// Data we are sending to the server.
+		request_.set_csservice01method01messagein01param01(user);
+
+		// This is important: You can have at most one write or at most one read
+		// at any given time. The throttling is performed by gRPC completion
+		// queue. If you queue more than one write/read, the stream will crash.
+		// Because this stream is bidirectional, you *can* have a single read
+		// and a single write request queued for the same stream. Writes and reads
+		// are independent of each other in terms of ordering/delivery.
+		//std::cout << " ** Sending request: " << user << std::endl;
+		stream_->Write(request_, reinterpret_cast<void *>(Type::WRITE));
+		return true;
+	}
+
+private:
+	void AsyncHelloRequestNextMessage()
+	{
+
+		// The tag is the link between our thread (main thread) and the completion
+		// queue thread. The tag allows the completion queue to fan off
+		// notification handlers for the specified read/write requests as they
+		// are being processed by gRPC.
+		stream_->Read(&response_, reinterpret_cast<void *>(Type::READ));
+	}
+
+	// Runs a gRPC completion-queue processing thread. Checks for 'Next' tag
+	// and processes them until there are no more (or when the completion queue
+	// is shutdown).
+	void GrpcThread()
+	{
+		while (true)
+		{
+			void *got_tag;
+			bool ok = false;
+			// Block until the next result is available in the completion queue "cq".
+			// The return value of Next should always be checked. This return value
+			// tells us whether there is any kind of event or the cq_ is shutting
+			// down.
+			if (!cq_.Next(&got_tag, &ok))
+			{
+				std::cerr << "Client stream closed. Quitting" << std::endl;
+				break;
+			}
+
+			// It's important to process all tags even if the ok is false. One might
+			// want to deallocate memory that has be reinterpret_cast'ed to void*
+			// when the tag got initialized. For our example, we cast an int to a
+			// void*, so we don't have extra memory management to take care of.
+			if (ok)
+			{
+				//std::cout << std::endl << "**** Processing completion queue tag " << got_tag << std::endl;
+				switch (static_cast<Type>(reinterpret_cast<long>(got_tag)))
+				{
+				case Type::READ:
+					std::cout << "Read a new message:" << response_.csservice01method01messageout01param01() << std::endl;
+					break;
+				case Type::WRITE:
+					std::cout << "Sent message :" << request_.csservice01method01messagein01param01() << std::endl;
+					AsyncHelloRequestNextMessage();
+					break;
+				case Type::CONNECT:
+					std::cout << "Server connected." << std::endl;
+					break;
+				case Type::DONE:
+					std::cout << "writesdone sent,sleeping 5s" << std::endl;
+					stream_->Finish(&finish_status_, reinterpret_cast<void *>(Type::FINISH));
+					break;
+				case Type::FINISH:
+					std::cout << "Client finish status:" << finish_status_.error_code() << ", msg:" << finish_status_.error_message() << std::endl;
+					//context_.TryCancel();
+					cq_.Shutdown();
+					break;
+				default:
+					std::cerr << "Unexpected tag " << got_tag << std::endl;
+					assert(false);
+				}
+			}
+		}
+	}
+
+	// Context for the client. It could be used to convey extra information to
+	// the server and/or tweak certain RPC behaviors.
+	ClientContext context_;
+
+	// The producer-consumer queue we use to communicate asynchronously with the
+	// gRPC runtime.
+	CompletionQueue cq_;
+
+	// Out of the passed in Channel comes the stub, stored here, our view of the
+	// server's exposed services.
+	std::unique_ptr<CSService01::Stub> stub_;
+
+	// The bidirectional, asynchronous stream for sending/receiving messages.
+	std::unique_ptr<ClientAsyncReaderWriter<CSService01Method01MessageIn01, CSService01Method01MessageOut01>> stream_;
+
+	// Allocated protobuf that holds the response. In real clients and servers,
+	// the memory management would a bit more complex as the thread that fills
+	// in the response should take care of concurrency as well as memory
+	// management.
+	CSService01Method01MessageIn01 request_;
+	CSService01Method01MessageOut01 response_;
+
+	// Thread that notifies the gRPC completion queue tags.
+	std::unique_ptr<std::thread> grpc_thread_;
+
+	// Finish status when the client is done with the stream.
+	grpc::Status finish_status_ = grpc::Status::OK;
+};
 
 void SignalHandler(int signal)
 {
-	LOG(LOG_LEVEL::DBG, "Received termination signal: %d", signal);
-	GClient::GetInstance().Stop();
+	cout << "Received termination signal: " << signal << endl;
 }
 
 int main(int argc, char **argv)
 {
+	cout << "Starting Client..." << endl;
+
 	signal(SIGTERM, SignalHandler);
 	signal(SIGINT, SignalHandler);
 	signal(SIGKILL, SignalHandler);
 
 	try
 	{
-		std::string grpc_server_address = "localhost:44444";
-		char *env_var = std::getenv("GRPC_SERVER_ADDRESS");
-		if (env_var != NULL)
-			grpc_server_address.assign(env_var);
+		uint64_t counter = 1;
 
-		g_system_log_level = 2;
-		env_var = std::getenv("LOGGING_LEVEL");
-		if (env_var != NULL)
+		AsyncBidiBiDiStreamClient greeter(grpc::CreateChannel(
+			"localhost:44444", grpc::InsecureChannelCredentials()));
+
+		std::string text;
+		while (true)
 		{
-			std::string temp(env_var);
-			g_system_log_level = std::stoi(temp);
+			text = to_string(counter++);
+
+			// Async RPC call that sends a message and awaits a response.
+			if (!greeter.AsyncSayHello(text))
+			{
+				std::cout << "Quitting." << std::endl;
+				break;
+			}
+
+			sleep(1);
 		}
-
-		LOG(LOG_LEVEL::INF, "GRPC Client starting...");
-		LOG(LOG_LEVEL::INF, "GRPC_SERVER_ADDRESS: %s", grpc_server_address.c_str());
-		LOG(LOG_LEVEL::INF, "LOGGING_LEVEL: %d", g_system_log_level);
-
-		if (!GClient::GetInstance().Initialize(grpc_server_address))
-		{
-            LOG(LOG_LEVEL::ERR, "Client initialization failed.");
-			return -1;
-		}
-
-		GClient::GetInstance().Run();
-	}
+	} 
 	catch (std::exception &e)
 	{
-		LOG(LOG_LEVEL::ERR, "Exception: %s", e.what());
+		cout << "Exception: " << e.what() << endl;
 		return -1;
 	}
 
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,59 +1,204 @@
+// https://gist.github.com/ppLorins/6e4cc625c2c5b8fd16ced3172b1ada09
+
 #include <csignal>
 #include <iostream>
 
-#include "gserver.h"
-#include "log_helper.h"
+#include <memory>
+#include <string>
+#include <thread>
+#include <grpc++/grpc++.h>
+#include "client_server_bidi_async_stream.grpc.pb.h"
 
 using namespace std;
 using namespace client_server::grpc::v1;
 
-extern int g_system_log_level;
+using grpc::Server;
+using grpc::ServerAsyncReaderWriter;
+using grpc::ServerBuilder;
+using grpc::ServerCompletionQueue;
+using grpc::ServerContext;
+using grpc::Status;
+
+enum class Type
+{
+	READ = 1,
+	WRITE = 2,
+	CONNECT = 3,
+	DONE = 4,
+	FINISH = 5
+};
+
+class AsyncBidiBiDiStreamServer
+{
+public:
+	AsyncBidiBiDiStreamServer()
+	{
+		// In general avoid setting up the server in the main thread (specifically,
+		// in a constructor-like function such as this). We ignore this in the
+		// context of an example.
+		std::string server_address("0.0.0.0:44444");
+
+		ServerBuilder builder;
+		builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+		builder.RegisterService(&service_);
+		cq_ = builder.AddCompletionQueue();
+		server_ = builder.BuildAndStart();
+
+		// This initiates a single stream for a single client. To allow multiple
+		// clients in different threads to connect, simply 'request' from the
+		// different threads. Each stream is independent but can use the same
+		// completion queue/context objects.
+		stream_.reset(
+			new ServerAsyncReaderWriter<CSService01Method01MessageOut01, CSService01Method01MessageIn01>(&context_));
+
+		service_.RequestCSServiceMethod01(&context_,
+										  stream_.get(),
+										  cq_.get(),
+										  cq_.get(),
+										  reinterpret_cast<void *>(Type::CONNECT));
+
+		// This is important as the server should know when the client is done.
+		context_.AsyncNotifyWhenDone(reinterpret_cast<void *>(Type::DONE));
+
+		grpc_thread_.reset(new std::thread(
+			(std::bind(&AsyncBidiBiDiStreamServer::GrpcThread, this))));
+		std::cout << "Server listening on " << server_address << std::endl;
+	}
+
+	void SetResponse(const std::string &response)
+	{
+		if (response == "quit" && IsRunning())
+		{
+			stream_->Finish(grpc::Status::CANCELLED,
+							reinterpret_cast<void *>(Type::FINISH));
+		}
+		response_str_ = response;
+	}
+
+	~AsyncBidiBiDiStreamServer()
+	{
+		std::cout << "Shutting down server...." << std::endl;
+		server_->Shutdown();
+		// Always shutdown the completion queue after the server.
+		cq_->Shutdown();
+		grpc_thread_->join();
+	}
+
+	bool IsRunning() const { return is_running_; }
+
+private:
+	void AsyncWaitForHelloRequest()
+	{
+		if (IsRunning())
+		{
+			// In the case of the server, we wait for a READ first and then write a
+			// response. A server cannot initiate a connection so the server has to
+			// wait for the client to send a message in order for it to respond back.
+			stream_->Read(&request_, reinterpret_cast<void *>(Type::READ));
+		}
+	}
+
+	void AsyncHelloSendResponse()
+	{
+		std::cout << " ** Handling request: " << request_.csservice01method01messagein01param01() << std::endl;
+		CSService01Method01MessageOut01 response;
+		std::cout << " ** Sending response: " << response_str_ << std::endl;
+		response.set_csservice01method01messageout01param01(response_str_);
+		stream_->Write(response, reinterpret_cast<void *>(Type::WRITE));
+	}
+
+	void GrpcThread()
+	{
+		while (true)
+		{
+			void *got_tag = nullptr;
+			bool ok = false;
+			if (!cq_->Next(&got_tag, &ok))
+			{
+				std::cerr << "Server stream closed. Quitting" << std::endl;
+				break;
+			}
+
+			//assert(ok);
+
+			if (ok)
+			{
+				std::cout << std::endl
+						  << "**** Processing completion queue tag " << got_tag
+						  << std::endl;
+				switch (static_cast<Type>(reinterpret_cast<size_t>(got_tag)))
+				{
+				case Type::READ:
+					std::cout << "Read a new message." << std::endl;
+					AsyncHelloSendResponse();
+					break;
+				case Type::WRITE:
+					std::cout << "Sending message (async)." << std::endl;
+					AsyncWaitForHelloRequest();
+					break;
+				case Type::CONNECT:
+					std::cout << "Client connected." << std::endl;
+					AsyncWaitForHelloRequest();
+					break;
+				case Type::DONE:
+					std::cout << "Server disconnecting." << std::endl;
+					is_running_ = false;
+					break;
+				case Type::FINISH:
+					std::cout << "Server quitting." << std::endl;
+					break;
+				default:
+					std::cerr << "Unexpected tag " << got_tag << std::endl;
+					assert(false);
+				}
+			}
+		}
+	}
+
+private:
+	CSService01Method01MessageIn01 request_;
+	std::string response_str_ = "Default server response";
+	ServerContext context_;
+	CSService01::AsyncService service_;
+	std::unique_ptr<ServerCompletionQueue> cq_;
+	std::unique_ptr<Server> server_;
+	std::unique_ptr<ServerAsyncReaderWriter<CSService01Method01MessageOut01, CSService01Method01MessageIn01>> stream_;
+	std::unique_ptr<std::thread> grpc_thread_;
+	bool is_running_ = true;
+};
 
 void SignalHandler(int signal)
 {
-	LOG(LOG_LEVEL::DBG, "Received termination signal: %d", signal);
-	GServer::GetInstance().Stop();
+	cout << "Received termination signal: " << signal << endl;
 }
 
 int main(int argc, char **argv)
 {
+	cout << "Starting Server..." << endl;
+
 	signal(SIGTERM, SignalHandler);
 	signal(SIGINT, SignalHandler);
 	signal(SIGKILL, SignalHandler);
 
 	try
 	{
-		g_system_log_level = 2;
-		
-		char *env_var = std::getenv("LOGGING_LEVEL");
-		if (env_var != NULL)
+		AsyncBidiBiDiStreamServer server;
+
+		int64_t counter = -1;
+
+		std::string response;
+		while (server.IsRunning())
 		{
-			std::string temp(env_var);
-			g_system_log_level = std::stoi(temp);
+			response = to_string(counter--);
+			cout << "Server sending: " << response << endl;
+			server.SetResponse(response);
+
+			sleep(1);
 		}
-
-		// Should improve arg parsing...
-		std::string grpc_server_address{"0.0.0.0:44444"};
-		if (argc >= 2)
-			grpc_server_address = argv[1];
-
-
-		LOG(LOG_LEVEL::INF, "GRPC Server starting...");
-		LOG(LOG_LEVEL::INF, "GRPC_SERVER_ADDRESS: %s", grpc_server_address.c_str());
-		LOG(LOG_LEVEL::INF, "LOGGING_LEVEL: %d", g_system_log_level);
-
-
-		if (!GServer::GetInstance().Initialize(grpc_server_address))
-		{
-			LOG(LOG_LEVEL::ERR, "Server initialization failed.");
-			return -1;
-		}
-
-		GServer::GetInstance().Run();
 	}
 	catch (std::exception &e)
 	{
-		LOG(LOG_LEVEL::ERR, "Exception: %s", e.what());
+		cout << "Exception: " << e.what() << endl;
 		return -1;
 	}
 
